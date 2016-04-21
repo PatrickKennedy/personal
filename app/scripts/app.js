3ff72d8eb830ba54f3191b05639b9330c468eef7
Patrick Kennedy <pakattack161@gmail.com>
2016-04-21 01:18:58 -0600

    add nav routing, including demo template support

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,13 @@
 (function(angular){
   angular
     .module('personal', [
-      'ngRoute', 'personal.templates', 'personal.home', 'personal.about',
+      'ngRoute',
+      'personal.templates',
+      'personal.about',
+      'personal.contact',
+      'personal.home',
+      'personal.portfolio',
+      'personal.resume',
     ])
     .controller('PersonalController', PersonalController)
     .config(RouteConfig)
@@ -13,6 +19,28 @@
       .when('/about', {
         templateUrl: 'about.jade',
         controller: 'AboutCtrl',
+        controllerAs: 'ctrl',
+      })
+      .when('/contact', {
+        templateUrl: 'contact.jade',
+        controller: 'ContactCtrl',
+        controllerAs: 'ctrl',
+      })
+      .when('/portfolio', {
+        templateUrl: 'portfolio.jade',
+        controller: 'PortfolioCtrl',
+        controllerAs: 'ctrl',
+      })
+      .when('/portfolio/:project', {
+        templateUrl: function(url_attrs){
+          return url_attrs.project + '.jade';
+        },
+        controller: 'ProjectsCtrl',
+        controllerAs: 'ctrl',
+      })
+      .when('/resume', {
+        templateUrl: 'resume.jade',
+        controller: 'ResumeCtrl',
         controllerAs: 'ctrl',
       })
       .when('/', {
