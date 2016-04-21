960d5ae304cfaae6ee10712f918c051199196547
Patrick Kennedy <pakattack161@gmail.com>
2016-04-21 01:21:19 -0600

    add bare content for portfolio and contact

---
+++ app/scripts/personal.portfolio.js
@@ -0,0 +1,16 @@
+(function(angular){
+  angular
+    .module('personal.portfolio', [])
+    .controller('PortfolioCtrl', PortfolioCtrl)
+    .controller('ProjectsCtrl', ProjectsCtrl)
+  ;
+
+  PortfolioCtrl.$inject = [];
+  function PortfolioCtrl() {
+  }
+
+  ProjectsCtrl.$inject = [];
+  function ProjectsCtrl() {
+  }
+
+}(angular));
