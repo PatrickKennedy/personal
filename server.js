9835ba03505c349e85620b730aa331a3a97de1ef
Patrick Kennedy <pakattack161@gmail.com>
2016-04-28 23:17:08 -0600

    add a basic server

---
+++ server.js
@@ -0,0 +1,11 @@
+var connect = require('connect');
+var http = require('http');
+var serveStatic = require('serve-static');
+
+var app = connect();
+
+// respond to all requests
+app.use(serveStatic('release', {'index': ['index.html']}));
+
+//create node.js http server and listen on port
+http.createServer(app).listen(3000);
